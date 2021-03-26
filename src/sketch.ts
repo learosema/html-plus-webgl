8158da1d21fa29f25dceb7c4b8ffae9202a0c6e2
Lea Rosema <terabaud@gmail.com>
2021-03-26 22:44:23 +0100

    feat: add shaders

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,18 +1,20 @@
 import * as THREE from 'three';
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import vertexShader from './shaders/default.vert';
+import fragmentShader from './shaders/default.frag';
 type SketchOptions = {
   dom: HTMLCanvasElement;
 };
 
 export class Sketch {
   renderer: THREE.WebGLRenderer;
-  dom: HTMLCanvasElement;
+  container: HTMLCanvasElement;
   width: number;
   height: number;
-
+  controls: OrbitControls;
   scene = new THREE.Scene();
   geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
+  material = new THREE.ShaderMaterial({ vertexShader, fragmentShader });
   mesh = new THREE.Mesh(this.geometry, this.material);
   clock = new THREE.Clock();
   camera: THREE.PerspectiveCamera;
@@ -20,13 +22,13 @@ export class Sketch {
   frame = -1;
 
   constructor(options: SketchOptions) {
-    this.dom = options.dom;
+    this.container = options.dom;
     this.renderer = new THREE.WebGLRenderer({
       antialias: false,
     });
-    this.dom.appendChild(this.renderer.domElement);
-    this.width = this.dom.offsetWidth;
-    this.height = this.dom.offsetHeight;
+    this.container.appendChild(this.renderer.domElement);
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
 
     this.camera = new THREE.PerspectiveCamera(
       70,
@@ -34,6 +36,9 @@ export class Sketch {
       0.01,
       10
     );
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+
     this.onResize();
     window.addEventListener('resize', this.onResize, false);
     this.camera.position.z = 1;
@@ -62,9 +67,10 @@ export class Sketch {
 
   render = () => {
     const time = this.clock.getElapsedTime();
-    this.mesh.rotation.x = time * 1.2;
-    this.mesh.rotation.y = time * 1.5;
+    //this.mesh.rotation.x = time * 1.2;
+    // this.mesh.rotation.y = time * 1.5;
     this.renderer.render(this.scene, this.camera);
+    this.controls.update();
     this.frame = requestAnimationFrame(this.render);
   };
 
